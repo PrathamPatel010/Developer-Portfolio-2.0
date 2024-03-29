78158e8e0d273b7853f0a52b7e5608a4bf69855f
Pratham <ppratham812@gmail.com>
2024-03-29 15:44:55 +0530

    initialized with next.js template

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen bg-[#121212]">
+      <h1 className={'text-white'}>Portfolio Website</h1>
+    </main>
+  );
+}
